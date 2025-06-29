564f7aed350793acec96989abc17f1339c65b45d
anith kesava <anithkesava@gmail.com>
2025-06-29 15:18:14 +0530

    save until table

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const parent = document.getElementById('ParentCourseId');
 const courseInput = document.getElementById('coursenameId');
 const child_select = document.createElement('select');
+child_select.id = 'studentCourseNameId'
 const course = ['Select a Course','Java', 'Python', 'JavaScript', 'C#', 'SQL', 'Angular', 'React', 'TypeScript'];
 course.forEach((c) => {
     const child_option = document.createElement('option');
@@ -9,9 +10,31 @@ course.forEach((c) => {
     } else {
         child_option.value = c;
     }
-    console.log(child_option.value)
     child_option.textContent = c;  
     child_select.appendChild(child_option);
 })
 parent.replaceChild(child_select, courseInput)
 child_select.classList.add('custom-input-box')
+
+
+const btn = document.getElementById('submitbtnId')
+let sName = document.getElementById('studentNameId')
+let sAge = document.getElementById('studentAgeId');
+let sCourseName = document.getElementById('studentCourseNameId')
+let sEmail = document.getElementById('studentEmailId')
+let students;
+
+btn.addEventListener('click', (e) => {
+    if (e.type == 'click') {
+        console.log('student name: ' + sName.value)
+        console.log('student age: ' + sAge.value)
+        console.log('student course: ' + sCourseName.value)
+        console.log('student email: ' + sEmail.value)
+    }
+    students = { studentname: sName.value, studentage: sAge.value, studentcourse: sCourseName.value, studentEmail: sEmail.value };
+    
+})
+
+//table content: 
+//
+
