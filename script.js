a9595043ff0cdb76eece9e33df9cd532b1e7ebac
anith kesava <anithkesava@gmail.com>
2025-06-30 07:47:39 +0530

    save

--- script.js
+++ script.js
@@ -23,73 +23,99 @@ let sEmail = document.getElementById('studentEmailId')
 let students;
 let deletebtn;
 btn.addEventListener('click', (e) => {
-    if (sName.value != '' && sAge.value != '' && sCourseName.value !='' && sEmail.value != '') {
-        if (e.type == 'click') {
-            console.log('student name: ' + sName.value)
-            console.log('student age: ' + sAge.value)
-            console.log('student course: ' + sCourseName.value)
-            console.log('student email: ' + sEmail.value)
-        }
-        students = { studentname: sName.value, studentage: sAge.value, studentcourse: sCourseName.value, studentEmail: sEmail.value };
-        const parenttbody = document.getElementById('studentDetailstbody')
-        const childtr = document.createElement('tr');
+    if (btn.innerText == 'Submit') {
+        if (sName.value != '' && sAge.value != '' && sCourseName.value != '' && sEmail.value != '') {
+            if (e.type == 'click') {
+                console.log('student name: ' + sName.value)
+                console.log('student age: ' + sAge.value)
+                console.log('student course: ' + sCourseName.value)
+                console.log('student email: ' + sEmail.value)
+            }
+            students = { studentname: sName.value, studentage: sAge.value, studentcourse: sCourseName.value, studentEmail: sEmail.value };
+            const parenttbody = document.getElementById('studentDetailstbody')
+            const childtr = document.createElement('tr');
 
-        //edit and delete btn creation =>
-        const editbtn = document.createElement('button');
-        editbtn.textContent = 'Edit';
-        editbtn.id = 'editbtnId';
+            //edit and delete btn creation =>
+            const editbtn = document.createElement('button');
+            editbtn.textContent = 'Edit';
+            editbtn.id = 'editbtnId';
 
-        deletebtn = document.createElement('button');
-        deletebtn.textContent = 'Delete';
-        deletebtn.id = 'deletebtnId';
+            deletebtn = document.createElement('button');
+            deletebtn.textContent = 'Delete';
+            deletebtn.id = 'deletebtnId';
 
-        editbtn.classList.add('edit-btn');
-        deletebtn.classList.add('delete-btn');
+            editbtn.classList.add('edit-btn');
+            deletebtn.classList.add('delete-btn');
 
-        //loop =>
-        Object.values(students).forEach((c) => {
-            let childtd = document.createElement('td');
-            childtd.textContent = c;
-            childtr.append(childtd);
-        })
+            //loop =>
+            Object.values(students).forEach((c) => {
+                let childtd = document.createElement('td');
+                childtd.textContent = c;
+                childtr.append(childtd);
+            })
 
-        //edit and delete btn cell creation =>
-        const edittd = document.createElement('td');
-        const deletetd = document.createElement('td');
+            //edit and delete btn cell creation =>
+            const edittd = document.createElement('td');
+            const deletetd = document.createElement('td');
 
-        //edit and delete btn added to the cell =>
-        edittd.append(editbtn);
-        deletetd.append(deletebtn);
+            //edit and delete btn added to the cell =>
+            edittd.append(editbtn);
+            deletetd.append(deletebtn);
 
-        //edit and delete btn cell added to the row in the table =>
-        childtr.append(edittd);
-        childtr.append(deletetd);       
+            //edit and delete btn cell added to the row in the table =>
+            childtr.append(edittd);
+            childtr.append(deletetd);
 
-        //overall child row in the table add to table =>
-        parenttbody.appendChild(childtr);
+            //overall child row in the table add to table =>
+            parenttbody.appendChild(childtr);
         
-        //delete btn function =>
-        deletebtn.addEventListener('click', () => {
-        console.log('whether this will execute automatically')
-        parenttbody.removeChild(childtr);        
-        })
+            //delete btn function =>
+            deletebtn.addEventListener('click', () => {                
+                parenttbody.removeChild(childtr);
+            })
         
-        //edit btn function =>
-        editbtn.addEventListener('click', () => {
-            console.log('edit button clicked')
-            const editbtnrow = editbtn.closest('tr'); //************* need to explore this line ***********************
-            const cells = editbtnrow.querySelectorAll('td');
+            //edit btn function =>
+            editbtn.addEventListener('click', () => {
+                console.log('edit button clicked')
+                const editbtnrow = editbtn.closest('tr'); //************* need to explore this line ***********************
+                const cells = editbtnrow.querySelectorAll('td');
 
-            document.getElementById('studentNameId').value = cells[0].textContent;
-            document.getElementById('studentAgeId').value =  cells[1].textContent;
-            document.getElementById('studentCourseNameId').value = cells[2].textContent;
-            document.getElementById('studentEmailId').value = cells[3].textContent;  
+                document.getElementById('studentNameId').value = cells[0].textContent;
+                document.getElementById('studentAgeId').value = cells[1].textContent;
+                document.getElementById('studentCourseNameId').value = cells[2].textContent;
+                document.getElementById('studentEmailId').value = cells[3].textContent;
             
-        })
+                document.getElementById('submitbtnId').textContent = 'Update';
+            })
         
+        } else {
+            console.log('some fields are empty')
+        }
     } else {
-        console.log('some fields are empty')
+        console.error('button is currently not in submitting')
     }    
+})
+
+btn.addEventListener('click', () => {
+    if (btn.innerText == 'Update') {
+        /*
+        what needs to be done:
+        when the user click the button then the old data needs to be replace by the new one. 
+        so we want two things => 1. old data, 2. new data
+        */
+        
+        //new data =>
+        let name = document.getElementById('studentNameId').value;
+        let age = document.getElementById('studentAgeId').value;
+        let coursename = document.getElementById('studentCourseNameId').value;
+        let newemail = document.getElementById('studentEmailId').value; 
+        
+        cells[0].textContent = name;
+        cells[1].textContent = age;
+        cells[2].textContent = coursename;
+        cells[3].textContent = newemail;
+        
+    }
 })
 
 const showbtn = document.getElementById('showtableId');
